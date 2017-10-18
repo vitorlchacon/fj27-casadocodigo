238225254a8caad743802b838102cea777a0b5ec
Vitor Chacon <Vitor Chacon>
2017-10-18 19:23:33 -0200

    11.6: Configurando a segurança com o banco de dados

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.daos.UserDAO;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserDAO users;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
@@ -18,5 +26,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated()
 		.and()
 		.formLogin();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(users).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
